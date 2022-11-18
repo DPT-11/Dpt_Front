86850a377195a6e2b59738c7495d32475a5135a7
Hyomk <qet557@gmail.com>
2022-11-19 00:48:21 +0900

    feat : 헤더 decoration 에니메이션

--- src/components/Decoration/index.js
+++ src/components/Decoration/index.js
@@ -1,8 +1,28 @@
 import React from "react";
 import DecorationImg from "../../assets/decoration.png";
 
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 
+import Deco1 from "../../assets/deco1.png";
+import Deco2 from "../../assets/deco2.png";
+import Deco3 from "../../assets/deco3.png";
+
+const animateDeco = keyframes` 
+    0% {
+        transform: translate(0%, 0%);
+        transition: transform 0.3s;
+    }
+
+    50% {
+        transform: translate(0%, -5%);
+        transition: transform 0.3s;
+       
+    }
+    100% {
+        transform: translate(0%,0%);
+        transition: transform 0.3s;
+    }
+`;
 const StyledDecorationDiv = styled.div`
     position: absolute;
     width: 100%;
@@ -13,12 +33,66 @@ const StyledDecorationDiv = styled.div`
     top: -20px;
 `;
 
+const StyledDecorationAnimateDiv = styled.div`
+    position: absolute;
+    z-index: 0;
+    width: 60%;
+    height: 15rem;
+    display: flex;
+    flex-direction: row;
+    top: -3rem;
+    justify-content: space-around;
+
+    div {
+        z-index: 0;
+        width: 100%;
+        background-repeat: no-repeat;
+        background-position: top center;
+        background-size: contain;
+        animation: ${animateDeco} 1.3s linear 0s;
+        animation-iteration-count: infinite;
+    }
+`;
+
 const Decoration = () => {
     return (
-        <StyledDecorationDiv
-            style={{ backgroundImage: `url(${DecorationImg})` }}
-        ></StyledDecorationDiv>
+        <StyledDecorationAnimateDiv>
+            <div
+                style={{
+                    animationDelay: "0.2s",
+                    backgroundImage: `url(${Deco2})`,
+                }}
+            >
+                {" "}
+            </div>
+            <div
+                style={{
+                    animationDelay: "0.3s",
+                    backgroundImage: `url(${Deco1})`,
+                }}
+            >
+                {" "}
+            </div>
+
+            <div
+                style={{
+                    marginTop: "100px !important",
+                    animationDelay: "0.1s",
+                    backgroundImage: `url(${Deco3})`,
+                }}
+            >
+                {" "}
+            </div>
+        </StyledDecorationAnimateDiv>
     );
 };
+
+// const Decoration = () => {
+//     return (
+//         <StyledDecorationDiv
+//             style={{ backgroundImage: `url(${DecorationImg})` }}
+//         ></StyledDecorationDiv>
+//     );
+// };
 
 export default Decoration;
