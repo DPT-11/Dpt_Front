e45cdf772721874dd395def5319380c20fcd3a0f
Hyomk <qet557@gmail.com>
2022-11-19 18:12:34 +0900

    feat : 이름 임시추가

--- src/components/kjwStart.js
+++ src/components/kjwStart.js
@@ -1,20 +1,28 @@
-import ovenGroup from "../img/ovenGroup1.png"
+import ovenGroup from "../img/ovenGroup1.png";
 import KjwImgHeader from "./kjwImgHeader";
-import KjwStartNameForm from './kjwStartNameForm';
 import KjwStartContent from "./kjwStartContent";
+import KjwStartNameForm from "./kjwStartNameForm";
 
-function KjwStart(props){
+function KjwStart(props) {
     return (
         <>
             <KjwImgHeader />
             <div className="kjw_start_title">네가 만든 쿠키</div>
-            <KjwStartContent name={props.name}/>
-            <div style={{width:"100%",alignItem:"left", marginBottom:"70px" }}>
-                <img src={ovenGroup} alt="오븐 쿠키"/>
+            <KjwStartContent name={"김진우"} />
+            <div
+                style={{
+                    width: "100%",
+                    alignItem: "left",
+                    marginBottom: "70px",
+                }}
+            >
+                <img src={ovenGroup} alt="오븐 쿠키" />
             </div>
-            <KjwStartNameForm owner={props.owner} placeholder="이름을 입력해주세요"/>
+            <KjwStartNameForm
+                owner={props.owner}
+                placeholder="이름을 입력해주세요"
+            />
         </>
-        
     );
 }
 export default KjwStart;
